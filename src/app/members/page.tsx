54cab4507b833292514b2daf2d6421943de4f6ba
Tomas Dang <tomdang@email.cz>
2024-07-30 22:00:20 +0200

    fix header import, add members page

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,10 +1,39 @@
 import User from "@/models/User";
 import dbConnect from "@/db/dbConnect";
+import PlayerCardBig from "@/components/PlayerCards/PlayerCardBig";
+import PlayerCardSmall from "@/components/PlayerCards/PlayerCardSmall";
 
 dbConnect();
 
 export default async function Members() {
-    const users = await User.find({}).sort("name");
+    let users = await User.find({}).sort("name");
+    const coach = await User.findOne({ _id: "668d082fbb6894b643abeb6b" });
+    users = users.filter(
+        (user) =>
+            !user._id.equals(coach._id) &&
+            !user._id.equals("669e2e4da78779b9287aaafd"),
+    );
+    console.log(users[0]._id);
 
-    return <h1>Test members</h1>;
+    return (
+        <>
+            <div className="mt-24">
+                <PlayerCardBig
+                    name={coach.name}
+                    profilePicSrc={coach.profilePicSrc}
+                />
+            </div>
+            <div className="box my-5 py-3">
+                {users.map((user) => {
+                    return (
+                        <PlayerCardSmall
+                            key={user._id}
+                            name={user.name}
+                            profilePicSrc={user.profilePicSrc}
+                        />
+                    );
+                })}
+            </div>
+        </>
+    );
 }
---
+++ src/components/PlayerCards/PlayerCardSmall.tsx
@@ -0,0 +1,13 @@
+export default function PlayerCardSmall(props: {
+    name: string;
+    profilePicSrc: string;
+}) {
+    return (
+        <div className="flex w-full flex-row items-center justify-between px-6 py-3">
+            <div className="h-11 w-11 rounded-full">
+                <img className="rounded-full" src={props.profilePicSrc}></img>
+            </div>
+            <h4 className="flex-1 pl-4">{props.name}</h4>
+        </div>
+    );
+}
