b5418d4d0c69769ad3fb0a2522d1b96f9039a29a
Tomas Dang <tomdang@email.cz>
2024-08-26 20:37:22 +0200

    change price in prices page

--- src/app/prices/page.tsx
+++ src/app/prices/page.tsx
@@ -3,7 +3,7 @@ export default function Prices() {
         <>
             <div className="box mt-[25vh] items-start p-4">
                 <h2 className="text-xl font-bold">Standa</h2>
-                <span className="pb-4 pt-1">500,-</span>
+                <span className="pb-4 pt-1">500,- (od 1.9.2024 600,-)</span>
                 <h2 className="text-xl font-bold">Cena kurtu do 15h</h2>
                 <span className="pb-4 pt-1">210,-</span>
                 <h2 className="text-xl font-bold">Cena kurtu od 15h</h2>
